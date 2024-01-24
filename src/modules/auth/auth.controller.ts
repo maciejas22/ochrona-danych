e84cc270a09a7a652cff62c4cec5d08ca86dc55f
Maciej Lewandowski <motorosas2012@gmail.com>
2024-01-24 18:49:35 +0100

    change cookie to secure

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -110,7 +110,7 @@ export async function loginUserHandler(
         domain: 'localhost',
         path: '/',
         sameSite: 'strict',
-        secure: false,
+        secure: true,
         httpOnly: true,
         expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000), // 1 day
       })
